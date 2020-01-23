30d6bcaae3968ca8af076ad82246effe9305aafc
ophelie <noel.ophelie@gmail.com>
2020-01-23 21:31:39 +0100

    V3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,10 +12,10 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,24 +4,36 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('date')
-            ->add('descriptif')
-        ;
+
+            ->add('titre',TextType::class,['label'=>'titre','attr'=>['class'=>'form-control'],])
+            ->add('date',DateTimeType::class,['label'=>'date', 'placeholder' => [
+                'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+                'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
+            ],])
+            ->add('descriptif',TextareaType::class,['label'=>'Descriptif','attr'=>['class'=>'form-control'],])
+
+            ->add('submit', SubmitType::class)
+            ->getform();
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
+
         ]);
     }
 }
